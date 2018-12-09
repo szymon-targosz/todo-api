98b4d2ddcdb50af5227dee9f348102d81f99b89f
Szymon Targosz <szymon7.t@gmail.com>
2018-12-09 12:15:08 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-if(process.env.NODE_ENV === 'production') {
-    require('dotenv').config();
-}
-
 mongoose.Promise = global.Promise; 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }); 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }); 
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
@@ -7,7 +8,7 @@ const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
 
 const app = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
