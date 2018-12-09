a23510cdb5c25d51a765461ac3a04a7d0c25d0ae
Szymon Targosz <szymon7.t@gmail.com>
2018-12-09 21:43:10 +0100

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,54 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
         minlength: 1,
-        required: true
-    }
+        required: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: props => `${props.value} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.generateAuthToken = function () {
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+
+    user.tokens.push({ access, token });
+
+    return user.save().then(() => token);
+};
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -106,6 +106,23 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+
+app.post('/users', (req, res) => {
+    const user = new User(_.pick(req.body, ['email', 'password']));
+
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then(token => {
+            res.header('x-auth', token).send(user);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+});
+
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}.`);
 });
