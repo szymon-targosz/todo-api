a95b7b320a2b735f3870b05f96e0720b66418dd0
Szymon Targosz <szymon7.t@gmail.com>
2018-12-11 19:58:36 +0100

    Add tests for POST /users/login

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development'; 
 
 if (env === 'development') {
-    process.env.PORT = 3001;
+    process.env.PORT = 3002;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
     process.env.PORT = 3010;
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -206,7 +206,7 @@ describe('GET /users/me', () => {
             .set('x-auth', users[0].tokens[0].token)
             .expect(200)
             .expect(res => {
-                expect(res.body._id).toBe(users[0]._id.toHexString()); 
+                expect(res.body._id).toBe(users[0]._id.toHexString());
                 expect(res.body.email).toBe(users[0].email);
             })
             .end(done);
@@ -214,7 +214,7 @@ describe('GET /users/me', () => {
 
     it('should return 401 if not authenticated', done => {
         request(app)
-            .get('/users/me') 
+            .get('/users/me')
             .expect(401)
             .expect(res => {
                 expect(res.body).toEqual({});
@@ -249,7 +249,7 @@ describe('POST /users/signup', () => {
                 }).then(user => {
                     expect(user).toExist();
                     expect(user.email).toBe(email);
-                    expect(user.password).toNotBe(password); 
+                    expect(user.password).toNotBe(password);
                     done();
                 }).catch(err => done());
             });
@@ -258,7 +258,7 @@ describe('POST /users/signup', () => {
     it('should return validation error if request is invalid', done => {
         request(app)
             .post('/users/signup')
-            .send({ email: 'mas', password: '123'})
+            .send({ email: 'mas', password: '123' })
             .expect(400)
             .end(done);
     });
@@ -269,6 +269,57 @@ describe('POST /users/signup', () => {
             .send({ email: users[0].email, password: '1234567' })
             .expect(400)
             .end(done);
+    });
+
+});
+
+describe('POST /users/login', () => {
+
+    it('should login user and return auth token', done => {
+        request(app)
+            .post('/users/login')
+            .send({ email: users[1].email, password: users[1].password })
+            .expect(200)
+            .expect(res => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id)
+                    .then(user => {
+                        expect(user.tokens[0]).toInclude({
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        });
+                        done();
+                    })
+                    .catch(err => done(err));
+            });
+    });
+
+    it('should reject invalid login', done => {
+        request(app)
+            .post('/users/login')
+            .send({ email: users[1].email, password: users[1].email + '11' })
+            .expect(400)
+            .expect(res => {
+                expect(res.headers['x-auth']).toNotExist()
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id)
+                    .then(user => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    })
+                    .catch(err => done(err));
+            });
     });
 
 });
